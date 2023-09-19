c86036296e1ee44160ca9efe30c34a3819f3cd0e
somorup <79506796+somorup@users.noreply.github.com>
2023-09-19 06:27:27 +0000

    Conversion

---
+++ Basic/convertions.js
@@ -0,0 +1,24 @@
+//it takes the first operation in consideration while processing
+/*console.log("2"+1+5); //output= 215
+console.log(1+5+"2"); //output=62
+
+console.log(true)
+console.log(+true) //+ conerts to number
+console.log(+false)
+
+let num1,num2,num3
+num1=num2=num3 = 10
+console.log(num2);
+
+//Prefix an Postfix operator
+let a=1
+let b=++a
+console.log(b);
+let c= a++
+console.log(c);//instead of 3 the output is 2, since post opeartion, the increment happens*/
+
+//comparison
+console.log(null==0)//false
+console.log(null>=0)//true, because comparison operator converts null to number 0/NaN before operations unlike eqality operator
+
+console.log(null===0)//=== means it will check both the datatypes too and found not equal then false
