0446aeb36219f748114cf3f3436752fb5cdb89e4
somorup <79506796+somorup@users.noreply.github.com>
2023-10-06 20:43:09 +0000

    if

---
+++ ControlFlow/one.js
@@ -0,0 +1,6 @@
+//if control flow
+//if(condition is true) {controller shifts inside here}
+// == vs === , === checks the data type equality too along with value equality
+//if controller do not need {} if the body is of one line or comma separated line
+if(1==1) console.log("one"),console.log("is"),console.log(one);
+
