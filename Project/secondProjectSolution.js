236b679d0bc1fd6226f01905d1f92db2a3ac0131
somorup <79506796+somorup@users.noreply.github.com>
2023-12-27 06:12:44 +0000

    new

--- Project/secondProjectSolution.js
+++ Project/secondProjectSolution.js
@@ -5,12 +5,18 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', function(e) {
   e.preventDefault()
-  const height = parseInt(querySelector("#height").value)
-  const weight = parseInt(querySelector('#weight').value)
-  const results = querySelector('#results')
+  const height = parseInt(document.querySelector("#height").value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  const results = document.querySelector('#results')
 
   if(height===0||height<0||isNaN(height))
   {
     results.innerHTML = `${height} is not valid`
+  }else if(weight===0||weight<0||isNaN(weight))
+  {
+    results.innerHTML = `${weight} is not valid`
+  }else{
+    const bmi= (weight / ((height*height)/10000)).toFixed(2)
+    results.innerHTML = `<span>${bmi}</span>`
   }
 })
