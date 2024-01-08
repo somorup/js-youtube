47cf79fef73ba6fcd84f1cfcccd63198fad80ad3
somorup <79506796+somorup@users.noreply.github.com>
2024-01-08 18:10:12 +0000

    newApi

---
+++ Advanced/one.js
@@ -0,0 +1,4 @@
+//to see basic api -- api.github.com/users/somorup
+//to read complex api's use json formater, complex api-- https://randomuser.me/
+//previously we used to send XML HTTP Request/Ajax Request, it is heavily used in AJAX programming
+//
